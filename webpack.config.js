b5742581c9917af983b694879cb3fa32f949e98b
Ali Al Dallal <ali@alicoding.com>
2015-03-19 16:25:06 -0400

    Add structure directories for React and LESS

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack');
+
+var IMPORT_ES5_SHIM = 'imports?shim=es5-shim/es5-shim&' +
+                      'sham=es5-shim/es5-sham';
+
+function importEnvVars(keys) {
+  var result = {};
+
+  keys.forEach(function(key) {
+    if (typeof (process.env[key]) === 'string') {
+      result['process.env.' + key] = JSON.stringify(process.env[key]);
+    }
+  });
+
+  return result;
+}
+
+module.exports = {
+  entry: {
+    app: './templates/components/main.jsx',
+  },
+  devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: 'jsx-loader' },
+      // https://github.com/webpack/webpack/issues/558#issuecomment-60889168
+      { test: require.resolve('react'), loader: IMPORT_ES5_SHIM },
+      { test: require.resolve('react/addons'), loader: IMPORT_ES5_SHIM },
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin(importEnvVars([
+      // TODO: Define any variable we want to expose to the client here.
+    ]))
+  ]
+};
