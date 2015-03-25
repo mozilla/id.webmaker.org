f622d70a298c327a0c3f4d49855800d4b697014f
Kate Hudson <k88hudson@gmail.com>
2015-03-25 13:22:45 -0400

    progress

--- templates/components/header/header.test.jsx
+++ templates/components/header/header.test.jsx
@@ -1,21 +1,30 @@
 var should = require('should');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
+var RouterStub = require('react-router-stub');
 var Header = require('./header.jsx');
+
+var testProps = {
+  redirectText: 'Already have an account?',
+  redirectPage: 'login',
+  redirectLabel: 'login'
+};
 
 describe('header', function() {
 
   var instance;
 
-  describe('with a container reference required', function() {
-    beforeEach(function() {
-      instance = TestUtils.renderIntoDocument(<Header />);
-    });
-
-    it('should contain the words "Sign up"', function() {
-      var signUpEl = instance.refs.signUp.getDOMNode();
-      should(signUpEl.innerHTML).equal('Sign up');
-    });
+  beforeEach(function() {
+    instance = RouterStub.render(Header, testProps);
   });
-});
+  afterEach(function () {
+    RouterStub.unmount(instance);
+  });
 
+  it('should contain the redirectText', function () {
+    console.log(instance);
+    var signUpEl = instance.refs.signUp.getDOMNode();
+    should(signUpEl.innerHTML).equal('Sign up');
+  });
+
+});
