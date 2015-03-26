d8c5b2d1b960de3fb23aa9928dbe6ad93041cbf2
Christopher De Cairos <chris@chrisdecairos.ca>
2015-03-26 17:41:36 -0400

    cookie settings

--- web/index.js
+++ web/index.js
@@ -5,7 +5,8 @@ var options = {
   port: process.env.PORT,
   loginAPI: process.env.LOGINAPI,
   oauth_clients: process.env.OAUTH_DB ? JSON.parse(process.env.OAUTH_DB) : [],
-  cookieSecret: process.env.COOKIE_SECRET
+  cookieSecret: process.env.COOKIE_SECRET,
+  secureCookies: process.env.SECURE_COOKIES === true
 };
 
 var server = require('./server')(options);
