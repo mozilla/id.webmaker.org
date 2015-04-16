9e5d720e0fbe34140d1fbee26f5e43992fdf1872
Ali Al Dallal <ali@alicoding.com>
2015-04-16 14:34:30 -0400

    Username must be less than 20 characters long

--- templates/lib/validatorset.js
+++ templates/lib/validatorset.js
@@ -1,7 +1,7 @@
 var Joi = require('joi');
 
 var fieldValidators = {
-  username:       Joi.string().alphanum().min(1).required().label('Username'),
+  username:       Joi.string().alphanum().min(1).max(20).required().label('Username'),
   password:       Joi.string().regex(/^\S{8,128}$/).label('Password'),
   verifyPassword: Joi.any().valid(Joi.ref('password')).required().label('Password Confirmation'),
   email:          Joi.string().regex(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i).label('Email'),
