1fd670ca9183b2417b98dfa58cd8436b68c95c40
Jon Buckley <jon@jbuckley.ca>
2015-08-10 17:25:10 -0400

    Fixed migration SQL

--- migrations/0_add_allowed_responses.sql
+++ migrations/0_add_allowed_responses.sql
@@ -5,7 +5,7 @@ ADD COLUMN allowed_responses jsonb;
 
 UPDATE clients
 SET allowed_responses = '["code"]'::JSONB
-WHERE allowed_responses = NULL;
+WHERE allowed_responses IS NULL;
 
 ALTER TABLE clients
 ALTER COLUMN allowed_responses SET NOT NULL;
