fc71beb20504b34248d796cdefef7b8a30e77663
Christopher De Cairos <cade@mozillafoundation.org>
2015-11-30 09:41:21 -0500

    rename user import script

--- scripts/import_legacy_webmaker_users.js
+++ scripts/import_legacy_webmaker_users.js
@@ -1,11 +1,11 @@
 /* jshint esnext: true */
 
-// migrate_legacy_webmaker_users.js
+// import_legacy_webmaker_users.js
 // Imports users from a login.webmaker.org mysql database
 // run this script between migrations/1_add_user_tables.sql and migrations/2_add_user_fk.sql
 //
 // execute from the project root
-// node scripts/migrate_legacy_webmaker_users.js <WMLOGIN_DB_CONN_STRING> <WEBMAKER_DB_CONNSTRING>
+// node scripts/import_legacy_webmaker_users.js <WMLOGIN_DB_CONN_STRING> <WEBMAKER_DB_CONNSTRING>
 
 'use strict';
 
@@ -18,18 +18,18 @@ const webmakerConnString = process.argv[3];
 Hoek.assert(wmloginConnString, 'You must provide a connection string to a webmaker login database');
 Hoek.assert(webmakerConnString, 'You must provide a connection string to a webmaker database');
 
-let wmloginClient = knex({
+var wmloginClient = knex({
   client: 'mysql',
   connection: wmloginConnString
 });
 
-let webmakerClient = knex({
+var webmakerClient = knex({
   client: 'pg',
   connection: webmakerConnString
 });
 
 function insertRow(userRow) {
-  let webmakerRow = {
+  var webmakerRow = {
     id: userRow.id,
     email: userRow.email,
     username: userRow.username,
@@ -41,7 +41,7 @@ function insertRow(userRow) {
 }
 
 function addToWebmaker(userRows) {
-  return webmakerClient.transaction(function(txn) {
+  return webmakerClient.transaction((txn) => {
     Promise.all(userRows.map(insertRow))
     .then(txn.commit)
     .catch(txn.rollback);
@@ -52,11 +52,11 @@ wmloginClient('Users')
 .join('Passwords', 'Users.id', '=', 'Passwords.UserId')
 .select('Users.id', 'Users.email', 'Users.username', 'Users.prefLocale', 'Passwords.saltedHash')
 .then(addToWebmaker)
-.then(function() {
+.then(() => {
   console.log('All done!');
   process.exit(0);
 })
-.catch(function(err) {
+.catch((err) => {
   console.error("Error occured: ", err);
   process.exit(1);
 });
