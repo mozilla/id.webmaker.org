e7e5b001e34cc161055ac53e650edf6de6ffa88d
Christopher DeCairos <cade@mozillafoundation.org>
2019-02-20 16:31:09 -0500

    make that build:dev, and update scripts/build.js

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,7 @@ var buildCommand;
 if ( process.env.NPM_CONFIG_PRODUCTION === 'true' ) {
   buildCommand = 'build:production';
 } else {
-  buildCommand = 'build';
+  buildCommand = 'build:dev';
 }
 
 var npm = spawn('npm', ['run', buildCommand], {
